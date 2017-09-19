26d41cefbca0a5261c471c090cc11c48d1b61bdf
Kevin Altman <kevin@itsthatguy.com>
2017-09-19 11:00:57 -0400

    tabs to spaces...

--- __tests__/lib/config.test.ts
+++ __tests__/lib/config.test.ts
@@ -1,12 +1,12 @@
 import { Config } from '../../src/lib/config';
 
 describe('config', () => {
-	it('returns project config', () => {
-		expect(Config).toEqual({
-			contractsRoot: 'contracts/',
-			contractsPath: '**/*.contract.js',
-			compiledContractsRoot: 'contracts/json/',
-			producersContractsRoots: [],
-		});
-	});
+  it('returns project config', () => {
+    expect(Config).toEqual({
+      contractsRoot: 'contracts/',
+      contractsPath: '**/*.contract.js',
+      compiledContractsRoot: 'contracts/json/',
+      producersContractsRoots: [],
+    });
+  });
 });
